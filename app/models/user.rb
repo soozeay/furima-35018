78845332ef23e717a92bde154e5052cc66a084c4
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-23 21:46:54 +0900

    ユーザー登録のバリデーション追加
    パスワードは英数字6文字以上の制限を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :encrypted_password,:password,:password_confirmation, format: {with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
   with_options presence: true do
     validates :nickname
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
@@ -12,5 +13,5 @@ class User < ApplicationRecord
     validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birth_day
   end
-  
+
 end
