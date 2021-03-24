c831710ed4dfa86b86ced64dfc3f9fc33ec21732
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-24 12:28:44 +0900

    Userモデル並び替え
    バリデーションの中身を可読性を高める為並び替え

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
@@ -8,15 +6,18 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
+    validates :birth_day
+    
     with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."} do
       validates :last_name 
       validates :first_name
     end
+    
     with_options format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."} do
       validates :last_name_kana
       validates :first_name_kana
     end
-    validates :birth_day
+    
   end
 
 end
