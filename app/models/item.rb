dadbaecc5769efc3e5ca8491544ac7f922eac733
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-24 17:46:46 +0900

    itemモデルの修正
    修正箇所：ActiveHashについて

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,16 +4,16 @@ class Item < ApplicationRecord
     validates :desc
     validates :price
   end
-  belongs_to: user
+  belongs_to :user
   has_one_attached :image
 
-  extend Activehash::Associations::ActiveRecordExtentions
-  with_options numericality: {other_than: 0} do
-    belongs_to: category_id
-    belongs_to: status_id
-    belongs_to: shipping_fee_id
-    belongs_to: prefecture_id
-    belongs_to: esd_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  with_options presence: true, numericality: { other_than: 0 }  do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :esd_id
   end
 
 end
--- db/migrate/20210324053115_create_items.rb
+++ db/migrate/20210324053115_create_items.rb
@@ -4,12 +4,12 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :name,             null: false
       t.text :desc,               null: false
       t.integer :price,           null: false
-      t.references :user,         foreign_key: true
+      t.references :user,         null: false,foreign_key: true
       t.integer :category_id,     null: false
       t.integer :status_id,       null: false
       t.integer :shipping_fee_id, null: false
       t.integer :prefecture_id,   null: false
-      t.integer :esd,             null: false
+      t.integer :esd_id,          null: false
       t.timestamps
     end
   end
