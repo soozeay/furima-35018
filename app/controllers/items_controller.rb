0c5d835c9110fa5392d74e9a28a41ac0b48b8d52
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-24 17:53:37 +0900

    出品機能コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,23 @@ class ItemsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :desc, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :esd_id, :image).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "items#index"
   devise_for :users
+
+  resources :items, only: [:new, :create]
 end
