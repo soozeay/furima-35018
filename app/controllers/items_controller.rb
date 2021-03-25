261c33e9642178a9ed3ddd0f8ac8342dfdcae4c2
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-25 20:40:10 +0900

    編集ページへ遷移までを確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+      render :edit
+    else
+      redirect_to root_path
+    end
   end
 
   def update
