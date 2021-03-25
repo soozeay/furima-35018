8d256d8371db524486f2ac18bc8fd535f71aaf4a
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-25 15:40:56 +0900

    未ログイン状態ではトップページへ強制遷移させる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: :index
+  before_action :authenticate_user!, except: :index
 
   def index
   end
@@ -24,7 +24,4 @@ class ItemsController < ApplicationController
                                  :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
 end
