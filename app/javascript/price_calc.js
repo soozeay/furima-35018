6722fd6d801404a39a9ba05991b7ab89fdb905c9
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-25 11:32:17 +0900

    出品商品の手数料・利益の自動計算を実装

---
+++ app/javascript/price_calc.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',() => {
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener('input', () =>{
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addSaleProfit = document.getElementById("profit");
+    addTaxDom.innerHTML = inputValue * 0.1;
+    addSaleProfit.innerHTML = inputValue - inputValue * 0.1;
+  })
+});
