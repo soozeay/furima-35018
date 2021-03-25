b8069930d5f145a6aa62ff80627847e8e7f647d0
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-25 15:41:25 +0900

    Userモデルへ漏れているAssociationの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :items
+  
   validates :password, :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d)])[a-z\d]+\z/i }
 
   with_options presence: true do
