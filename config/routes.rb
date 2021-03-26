fc420c31e9eb6365e78a1240103476bff37ee06c
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-26 09:58:03 +0900

    コントローラーとルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "items#index"
   devise_for :users
 
-  resources :items, only: [:new, :create, :show, :edit, :update]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
 end
