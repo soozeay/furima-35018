39026c2375583008edeb1c3dab416cd773525d6a
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-27 21:41:52 +0900

    orderコントローラーの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,36 @@
 class OrdersController < ApplicationController
+before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_address = OrderAddress.new(order_params)
+    binding.pry
+    if @order_address.valid?
       pay_item
-      @order.save
+      @order_address.save
       return redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def order_params
-    params.require(:order).merge(token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id],)
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: set_item[:price],  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
