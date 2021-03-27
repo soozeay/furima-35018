277adb73291aa4bbf96b1094a5d94c4cf55e792b
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-27 21:41:05 +0900

    紐付く2つのモデルのバリデーションを修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,14 +1,3 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code
-    validates :city
-    validates :house_number
-    validates :phone_number
-  end
   belongs_to :order
-  belongs_to :prefecture
-
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :prefecture_id, presence: true, numericality: { other_than: 0 }
-
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   attr_accessor :token
   belongs_to :item
   belongs_to :user
+  has_one :address
 end
