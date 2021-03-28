bfe84ba66969a0a608980940b1306ba289b48832
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-28 19:01:51 +0900

    rubocopによる修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,18 @@
 class OrdersController < ApplicationController
-before_action :authenticate_user!
-before_action :set_item, only: [:index, :create]
-before_action :move_to_index
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index
 
   def index
-      @order_address = OrderAddress.new
+    @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_params)
-    binding.pry
     if @order_address.valid?
       pay_item
       @order_address.save
-      return redirect_to root_path
+      redirect_to root_path
     else
       render :index
     end
@@ -26,14 +25,16 @@ before_action :move_to_index
   end
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id],)
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(
+      token: params[:token], user_id: current_user.id, item_id: params[:item_id]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: set_item[:price],  # 商品の値段
-      card: order_params[:token],    # カードトークン
+      card: order_params[:token], # カードトークン
       currency: 'jpy'                 # 通貨の種類（日本円）
     )
   end
--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -7,7 +7,7 @@
 ActiveSupport.on_load(:action_controller) do
   wrap_parameters format: [:json]
 end
-
+Webpacker::Compiler.env["PAYJP_PUBLIC_KEY"] = ENV["PAYJP_PUBLIC_KEY"]
 # To enable root element in JSON for ActiveRecord objects.
 # ActiveSupport.on_load(:active_record) do
 #   self.include_root_in_json = true
