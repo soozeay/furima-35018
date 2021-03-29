460f6bc424e1114b7485314052ef2b0018692e26
soozeay <80019801+soozeay@users.noreply.github.com>
2021-03-29 11:13:28 +0900

    itemsコントローラーへアクセス制限の付与

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,8 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if @item.user_id != current_user.id
+    if @item.user_id != current_user.id or @item.order.present?
+      redirect_to root_path
+    end
   end
 end
