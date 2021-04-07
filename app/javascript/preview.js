733e74269ac45b344a5952e3b1f1776ea3eacd29
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-07 21:37:16 +0900

    リファクタリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,12 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const ImagePreview = document.getElementById('image-preview');
-  document.getElementById('item-image').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
-    const imageElement = document.createElement('div');
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
-    imageElement.appendChild(blobImage);
-    ImagePreview.appendChild(imageElement);
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImagePreview = document.getElementById('image-preview');
+
+    const createImageHTML = (blob) =>{
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      imageElement.appendChild(blobImage);
+      ImagePreview.appendChild(imageElement);
+    };
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+
+      if (imageContent){
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+      createImageHTML(blob);
+    });
+    
   });
-});
+}
