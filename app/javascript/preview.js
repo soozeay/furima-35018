7947797dc97a33521f1cda82fa03bde8ed0a2ade
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-07 21:12:48 +0900

    input要素で値の変化が起こったときにイベント発火

---
+++ app/javascript/preview.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ImagePreview = document.getElementById('image-preview');
+  document.getElementById('item-image').addEventListener('change', function(e){
+    console.log(123);
+  })
+})
