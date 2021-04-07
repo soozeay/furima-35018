88ed79e87326bb2d36c76b0b27784bbb8f29029a
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-07 19:29:38 +0900

    Revert "jsファイルの準備"
    This reverts commit f0aa4be04f258b142742b94e067ddbee03e74b5f.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("../price_calc")
 require("../card")
-require("..//preview")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
