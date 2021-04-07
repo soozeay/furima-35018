c67614bc8b3ff0e0d6a6ff3bcd2284838908da1c
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-07 21:27:24 +0900

    差し込むHTML要素の生成

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,5 +3,10 @@ document.addEventListener('DOMContentLoaded', function(){
   document.getElementById('item-image').addEventListener('change', function(e){
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
+    const imageElement = document.createElement('div');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('src', blob);
+    imageElement.appendChild(blobImage);
+    ImagePreview.appendChild(imageElement);
   });
 });
