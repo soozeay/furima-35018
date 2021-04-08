ee0a4c52d293dbdb5a9a61382a1eb643f7e537ae
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-08 20:21:09 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   root "items#index"
   devise_for :users
   resources :items do
     resources :orders
   end
   resources :users, only: [:show, :update]
+  resources :cards, only: [:new, :create]
 end
