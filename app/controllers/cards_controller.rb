a191840bedb6c127b21827ab1e1c2056236cca5d
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-08 20:31:05 +0900

    createアクションの記述

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,14 @@
 class CardsController < ApplicationController
+
   def new
   end
+ 
+  def create
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
+    customer = Payjp::Customer.create(
+    description: 'test', # テストカードであることを説明
+    card: params[:card_token] # 登録しようとしているカード情報
+    )
+  end
+  
 end
