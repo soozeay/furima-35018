9861530923a07593772fd677c0a64ebee1078333
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-08 18:38:23 +0900

    usersコントローラーとルーティング設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params) 
+      redirect_to root_path 
+    else
+      redirect_to "show"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders
   end
+  resources :users, only: [:show, :update]
 end
