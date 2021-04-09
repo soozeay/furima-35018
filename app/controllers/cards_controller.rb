5a3649bbf5e9e7c26e4795848643015dd4836bd4
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-09 09:47:17 +0900

    カード登録のコントローラーを記述

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,14 +6,22 @@ class CardsController < ApplicationController
   def create
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
     customer = Payjp::Customer.create(
-    description: 'test', # テストカードであることを説明
-    card: params[:card_token] # 登録しようとしているカード情報
+      description: 'test', # テストカードであることを説明
+      card: params[:card_token] # 登録しようとしているカード情報
     )
     card = Card.new( # トークン化されたカード情報を保存する
       card_token: params[:card_token], # カードトークン
       customer_token: customer.id, # 顧客トークン
       user_id: current_user.id # ログインしているユーザー
     )
+    binding.pry
+
+    if card.save
+      redirect_to user_path(current_user.id)
+    else
+      binding.pry
+      redirect_to "new"
+    end
   end
 
 end
