70f731808e6f51350a61307c65df4804e7cc6313
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-18 18:56:17 +0900

    バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
     validates :desc
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
+    validates :stock
   end
   belongs_to :user
   has_one_attached :image
