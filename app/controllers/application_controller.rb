594eb98b01f71fcc37a0387a6ae224453c454d8a
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-20 18:53:06 +0900

    current_cartの定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,12 @@ class ApplicationController < ActionController::Base
   end
 
   def current_cart
-    current_cart = Cart.find_or_create_by(id: session[:cart_id])
-    session[:cart_id] = current_cart.id
-    current_cart
+    if current_user
+      current_cart = Cart.find_or_create_by(user_id: current_user.id)
+    else
+      current_cart = Cart.find_or_create_by(id: session[:cart_id])
+      session[:cart_id] = current_cart.id
+    end
+      current_cart
   end
 end
