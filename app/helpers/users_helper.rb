165aec15f4557c2a7a7a644b4614bcc6225d5df9
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-21 15:14:02 +0900

    userページの各テンプレート修正
    処理はhelperに逃がす

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,19 @@
 module UsersHelper
+
+  def search_orders(item)
+    @order = Order.where(item_id: item.id)
+    return @order
+  end
+
+  def search_sold_item(order)
+    item = Item.find(order.item_id)
+    return item
+  end
+
+  def search_item(oi)
+    @item = Item.find(oi.item_id)
+    return @item
+  end
+
+
 end
