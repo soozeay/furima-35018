f9d65638230f2f7d86754add1fdf3af91de5304c
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-21 13:54:48 +0900

    不要な記述の削除

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,6 @@ class CartsController < ApplicationController
     @line_item = current_cart.lineitems.build(item_id: params[:item][:item_id])
     @line_item.quantity += params[:quantity].to_i
     @item = Item.find(params[:item][:item_id])
-    binding.pry
     if @line_item.valid? && @line_item.quantity <= @item.stock
       @line_item.save
       redirect_to item_path(@line_item.item_id)
