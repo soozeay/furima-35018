a39f5cb3d6a002ab1e60983a7999d2a7a5cec6fd
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-21 14:38:58 +0900

    tab.jsの有効化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../price_calc")
 require("../preview")
+require("../tab")
 require("../card_registration")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
