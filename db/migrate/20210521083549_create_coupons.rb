fe06e0bde79f9c35ab2b85ee96b8725c705fcd60
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-21 20:45:58 +0900

    クーポンモデルの生成

---
+++ db/migrate/20210521083549_create_coupons.rb
@@ -0,0 +1,10 @@
+class CreateCoupons < ActiveRecord::Migration[6.0]
+  def change
+    create_table :coupons do |t|
+      t.integer :content_id, null: false
+      t.references :user, null: false
+      t.boolean :is_valid, default: true
+      t.timestamps
+    end
+  end
+end
