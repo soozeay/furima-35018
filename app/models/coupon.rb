ab78db0975373e3328d0baafec03b77d504efb88
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-22 17:54:44 +0900

    モデルにクーポン生成と無効化を記述

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,22 @@
 class Coupon < ApplicationRecord
   belongs_to :user
+
+  enum is_valid: { '有効': true, '無効': false }
+
+  def self.coupon_create(user)
+    coupon = Coupon.new(user_id: user.id, limit: 1)
+    coupon.save
+  end
+
+  def self.coupon_destroy
+    time = Time.now
+    coupons = Coupon.all
+    coupons.each do |coupon|
+      if coupon.created_at + coupon.limit.days < time && coupon.is_valid == '有効'
+        coupon.is_valid = '無効'
+        coupon.save
+      end
+    end
+  end
+  
 end
