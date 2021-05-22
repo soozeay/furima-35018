eba4e6eabe1a508e17c8978b9f01879eee94c255
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-22 23:17:06 +0900

    クーポンの中身をアクティブハッシュで指定

---
+++ app/models/content.rb
@@ -0,0 +1,9 @@
+class Content < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '50% OFF' },
+    { id: 2, name: '10% OFF' },
+    { id: 3, name: '500円 OFF' }
+  ]
+  include ActiveHash::Associations
+  has_many :coupons
+end
