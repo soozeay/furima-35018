f1afd974fb456dbd920b8aeb9285e9b3e9bd4808
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-25 13:51:44 +0900

    一度コミットし他の対応へ

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,11 @@
 module OrdersHelper
+
+  def choose_coupons(user)
+    @coupons = user.coupons
+    @contents = []
+    @coupons.each do |coupon|
+      @contents << coupon.content.name
+    end
+    return @contents
+  end
 end
