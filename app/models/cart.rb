2f6c46bf7efa3b70fb1c3465f5da87d0129a13f8
soozeay <80019801+soozeay@users.noreply.github.com>
2021-05-30 10:17:01 +0900

    commit

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,14 +1,4 @@
 class Cart < ApplicationRecord
   has_many :lineitems, dependent: :destroy
   has_many :items, through: :lineitems
-
-  def current_cart
-    if current_user
-      current_cart = Cart.find_or_create_by(user_id: current_user.id)
-    else
-      current_cart = Cart.find_or_create_by(id: session[:cart_id])
-      session[:cart_id] = current_cart.id
-    end
-      current_cart
-  end
 end
